20d8e41e650c6527a220ce55fa177bc1e736f41a
Katrine Klochko <KatrineKlochko@yandex.ru>
2025-12-04 21:18:31 +0300

    add user tests

---
+++ src/test/java/ru/yandex/practicum/tests/UserLoginTests.java
@@ -0,0 +1,77 @@
+package ru.yandex.practicum.tests;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.junit4.DisplayName;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import ru.yandex.practicum.model.User;
+import ru.yandex.practicum.steps.UserSteps;
+import static org.hamcrest.CoreMatchers.is;
+
+public class UserLoginTests extends BaseTest{
+
+    private UserSteps userSteps = new UserSteps();
+    private User user;
+
+    @Before
+    public void setUp(){
+
+        user = new User();
+        user
+                .setEmail((RandomStringUtils.randomAlphabetic(12)) + "@ya.ru")
+                .setPassword(RandomStringUtils.randomAlphabetic(12))
+                .setName(RandomStringUtils.randomAlphabetic(12));
+        userSteps
+                .createUser(user);
+    }
+
+    @Test
+    @DisplayName("Проверка на логин пользователя")
+    @Description("Тест на успешный логин пользователя для /api/auth/login эндпоинт")
+    public void shouldLoginUserTest(){
+        userSteps
+                .loginUser(user)
+                .statusCode(200)
+                .body("success", is(true));
+    }
+
+    @Test
+    @DisplayName("Проверка на авторизацию пользователя с неверным паролем")
+    @Description("Тест на невозможность авторизации пользователя с неверным паролем для /api/auth/login эндпоинт")
+    public void shouldNotLoginUserWithNonExistentPasswordTest(){
+        User nonExistingUser = new User(user)
+                .setPassword(RandomStringUtils.randomAlphabetic(12));
+        userSteps
+                .loginUser(nonExistingUser)
+                .statusCode(401)
+                .body("message", is("email or password are incorrect"));
+    }
+
+    @Test
+    @DisplayName("Проверка на авторизацию пользователя с неверным логином")
+    @Description("Тест на невозможность авторизации пользователя с неверным логином для /api/auth/login эндпоинт")
+    public void shouldNotLoginUserWithNonExistentLoginTest(){
+        User nonExistingUser = new User(user)
+                .setEmail((RandomStringUtils.randomAlphabetic(12)) + "@ya.ru");
+        userSteps
+                .loginUser(nonExistingUser)
+                .statusCode(401)
+                .body("message", is("email or password are incorrect"));
+    }
+
+    @After
+    public void tearDown(){
+
+        String accessToken = userSteps
+                .loginUser(user)
+                .extract()
+                .body()
+                .path("accessToken");
+        if (accessToken != null) {
+            user.setAccessToken(accessToken);
+            userSteps.deleteUser(accessToken);
+        }
+    }
+}
